5029b2f669de7d7363675fe798bea26f207118db
Bhavii <142741751+Itz-bhavii@users.noreply.github.com>
2026-05-18 15:34:50 +0530

    created user entity

---
+++ spring-backend/src/main/java/com/bhavesh/taskflow/models/User.java
@@ -0,0 +1,39 @@
+package com.bhavesh.taskflow.models;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
